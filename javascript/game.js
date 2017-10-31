64491a96e8683e5b51e7c4b5fe8fe6f5edc2e6ae
mathewhall100 <mathew.hall@dr.com>
2017-10-31 00:34:27 -0400

    initial wireframe

---
+++ javascript/game.js
@@ -0,0 +1,46 @@
+
+
+window.onload = function () {  // check this is right  from book
+
+  var lettersArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+        't', 'u', 'v', 'w', 'x', 'y', 'z', '-' ];
+
+  Var contentArray = [ 
+  						['demogorgon', 'mythical beast', 'https://en.wikipedia.org/wiki/Demogorgon', 'https://cdnb.artstation.com/p/assets/images/images/003/749/943/original/limkuk-art-demogorgon-4.gif?1477078806'],
+  						[]
+  					 ];
+
+
+  var word ;              // Selected word
+  var guess ;             // Geuss
+  var geusses = [ ];      // Stored geusses
+  var lives ;             // Lives
+  var counter ;           // Count correct geusses
+  var space;              // Number of spaces in word '-'
+
+  // Get elements
+  var showLives = document.getElementById("mylives");
+  var showCatagory = document.getElementById("scatagory");
+  var getHint = document.getElementById("hint");
+  var showClue = document.getElementById("clue");
+
+
+
+  // create alphabet ul
+  var buttons = function () {
+    myButtons = document.getElementById('buttons');
+    letters = document.createElement('ul');
+
+    for (var i = 0; i < alphabet.length; i++) {
+      letters.id = 'alphabet';
+      list = document.createElement('li');
+      list.id = 'letter';
+      list.innerHTML = alphabet[i];
+      check();
+      myButtons.appendChild(letters);
+      letters.appendChild(list);
+    }
+  }
+
+}
